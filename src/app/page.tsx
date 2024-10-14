3c3ceb7275d3bbcd04e5bb7ba85ea53aaf2273a7
ogicodes <ogi@ogidesigns.ca>
2024-10-13 20:59:44 -0400

    created 3D card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import { Canvas } from "@react-three/fiber";
+import { Float, OrbitControls, useGLTF } from "@react-three/drei";
+import { Suspense } from "react";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+
+
+
+
+
+function Model() {
+  const { scene } = useGLTF("/UrMom.glb"); // path to your .glb file
+  return <primitive object={scene} scale={4.2} />;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+  return (
+    <main className="h-dvh w-dvh flex justify-center items-center relative">
+    <CardContainer className="inter-var text-white">
+      <CardBody className="bg-transparent border-[1px] border-white relative group/card w-auto sm:w-[30rem] h-auto rounded-xl p-6">
+        <CardItem
+          translateZ="200"
+          translateY={-30}
+          className="text-3xl font-bold"
+        >
+          Untitled&Co Shirt in Pink
+        </CardItem>
+        <CardItem
+          translateZ={300}
+          rotateX={20}
+          rotateZ={-10}
+          className="w-full mt-4"
+        >
+          <Canvas style={{ width: 375, height: 250 }}>
+            <Suspense fallback={null}>
+              <Float speed={2} rotationIntensity={2} floatIntensity={2}>
+              <ambientLight intensity={1.4} />
+              <directionalLight position={[5, 5, 5]} />
+              <Model />
+              <OrbitControls />
+              </Float>
+            </Suspense>
+          </Canvas>
+        </CardItem>
+        <div className="flex justify-between items-center mt-2">
+          <CardItem
+            translateZ={200}
+            translateY={40}
+            as="button"
+            className="px-4 py-2 rounded-xl text-xl font-bold dark:text-white"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            $208.00
+          </CardItem>
+          <CardItem
+            translateZ={200}
+            translateY={40}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-white  text-black text-md font-bold"
           >
-            Read our docs
-          </a>
+            Buy Now
+          </CardItem>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </CardBody>
+    </CardContainer>
+    <BackgroundBeams className="z-[-1] bg-black" />
+    </main>
   );
 }
